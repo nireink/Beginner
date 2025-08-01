7f4e32ccb1ec04bbb4435e891b0b2086ec03e676
Eduardo Burgasi <elburgasi@gmail.com>
2025-07-31 22:23:17 -0500

    Finalizacion de actualizacion de repositorio

---
+++ 02-intro-javascript/src/bases/01-const-let.jsx
@@ -0,0 +1,15 @@
+const nombre='Fernando';
+const apellido ='Herrera';
+
+let valorDado =5;
+valorDado=4;
+
+console.log(nombre, apellido, valorDado)
+
+if(true){
+    let valorDado = 6;
+
+    console.log(valorDado)
+}
+
+console.log(valorDado); 
